3a9e10bb71f8a479af48420648dffd3381591d73
Arsalan Sefidgar <a.sefidgar@pep.co.ir>
2024-01-20 14:57:51 +0330

    fix typo

--- backup/files/db_backup.sh
+++ backup/files/db_backup.sh
@@ -6,7 +6,7 @@ DEST=~/db_backups
 DB_HOST="127.0.0.1"
 DB_PORT=33306
 DB_USER="root"
-DB_PASS="'0hELsHHOhNNL2g8u5eL/L53BfQ9H3pD5P5QToX1Gw98='"
+DB_PASS="0hELsHHOhNNL2g8u5eL/L53BfQ9H3pD5P5QToX1Gw98="
 DB_NAME="WPDatabase"
 BUCKET_SUBDIR="sub"
 
@@ -18,7 +18,7 @@ then
 	exit 1
 fi
 
-nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER -p$DB_PASS --ignore-table=$DB_NAME.telescope_entries --single-transaction --opt --skip-lock-tables $DB_NAME | gzip > "$DEST"/dbbackup-$(date -I).gz
+nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER -p$DB_PASS --single-transaction --opt --skip-lock-tables $DB_NAME | gzip > "$DEST"/dbbackup-$(date -I).gz
 
 # ~/s3/mc cp $DEST/dbbackup-$(date -I).gz backup/g2-backup-dbs/$BUCKET_SUBDIR/
 
